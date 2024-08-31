f31f247f8994678476e3114eb3ca79508ea5df63
mohsinziia <ziamohsin409@gmail.com>
2024-08-31 16:15:18 +0500

    Fix logout functionality, Add comment, like, playlist, tweet models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -161,7 +161,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: { refreshToken: undefined },
+      $unset: { refreshToken: 1 },
     },
     { new: true }
   );
